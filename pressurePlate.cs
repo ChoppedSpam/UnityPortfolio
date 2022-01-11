4ac500e7203806d73b2cd8065ac440ea000d4302
ChoppedSpam <97447876+ChoppedSpam@users.noreply.github.com>
2022-01-11 09:20:02 +0000

    Add files via upload

---
+++ pressurePlate.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pressurePlate : MonoBehaviour
+{
+    public GameObject example;
+    public Animator anim;
+
+    List<GameObject> boxCount = new List<GameObject>();
+
+    void Start()
+    {
+        example.SetActive(true);
+    }
+
+    private void Update()
+    {
+        if (boxCount.Count > 0)
+        {
+            example.SetActive(false);
+            anim.Play("goDown");
+        }
+
+        if (boxCount.Count == 0)
+        {
+            example.SetActive(true);
+            anim.Play("goUp");
+        }
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        boxCount.Add(other.gameObject);
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        boxCount.Remove(other.gameObject);
+    }
+
+}
